60e616f8b37a84b4f06ab4f01cda965b769b624a
LinxinGuo <linxin.guo@yell.com>
2020-09-06 20:11:29 +0100

    Breadth first search

--- src/main/java/com/guo/practice/codepractice/problems/easy/BinaryTree.java
+++ src/main/java/com/guo/practice/codepractice/problems/easy/BinaryTree.java
@@ -1,5 +1,10 @@
 package com.guo.practice.codepractice.problems.easy;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+@Slf4j
 public class BinaryTree {
 
     // 2. start with root node
@@ -129,6 +134,125 @@ public class BinaryTree {
         return null;
     }
 
+
+    /**
+     * Remove value from tree ????????
+     *
+     * @return
+     */
+    public Node removeValue(int value) {
+        return deleteNodeRecursive(getRoot(), value);
+    }
+
+    private Node deleteNodeRecursive(Node node, int value) {
+
+        // TODO: 06/09/2020 try remove value in better way
+
+        if (node == null) {
+            return null;
+        }
+
+        if (value < node.getValue()) {
+            deleteNodeRecursive(node.getLeft(), value);
+            return node;
+        } else if (value > node.getValue()) {
+            deleteNodeRecursive(node.getRight(), value);
+            return node;
+        } else if (value == node.getValue()) {
+
+            // case 1 : node has no child
+            if (node.getRight() == null && node.getLeft() == null) {
+                node = null;
+                return null;
+            }
+
+            // case 2 : node has one child
+            if (node.getLeft() == null) {
+                return node.getRight();
+            }
+
+            if (node.getRight() == null) {
+                return node.getLeft();
+            }
+
+
+            // case 3 : node has 2 children
+
+            // find smallest value
+            int smallest = findSmallestValue(node);
+            node.setValue(smallest);
+            node.setRight(deleteNodeRecursive(node.getRight(), smallest));
+            return node;
+
+        }
+
+        return null;
+    }
+
+    private int findSmallestValue(Node node) {
+        return node.getLeft() == null ? node.getValue() : findSmallestValue(node.getLeft());
+    }
+
+
+    /**
+     * Breadth first search - BFS
+     *
+     * @return
+     */
+    public List<Integer> breadthFirstSearchLoop() {
+
+        // 1. use queue to insert / remove nodes and loop through all nodes
+        List<Integer> results = new ArrayList<>();
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        while (!nodeQueue.isEmpty()) {
+
+            Node removedNode = nodeQueue.poll();
+
+            results.add(removedNode.getValue());
+
+            if (removedNode.getLeft() != null) {
+                nodeQueue.add(removedNode.getLeft());
+            }
+
+            if (removedNode.getRight() != null) {
+                nodeQueue.add(removedNode.getRight());
+            }
+        }
+
+        log.debug("*** breadthFirstSearch : " + Arrays.toString(results.toArray()));
+        return results;
+
+    }
+
+    public List<Integer> bfsRecursive() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        return breadthFirstSearchRecursive(queue, new ArrayList<>());
+    }
+
+    private List<Integer> breadthFirstSearchRecursive(Queue<Node> queue, List<Integer> list) {
+        if (queue.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // remove first from queue. fifo
+        Node removedNode = queue.poll();
+        if (removedNode.getLeft() != null) {
+            list.add(removedNode.getValue());
+            queue.add(removedNode.getLeft());
+            return breadthFirstSearchRecursive(queue, list);
+        }
+
+        if (removedNode.getRight() != null) {
+            list.add(removedNode.getValue());
+            queue.add(removedNode.getRight());
+            return breadthFirstSearchRecursive(queue, list);
+        }
+
+        return list;
+    }
 
     public Node getRoot() {
         return root;
--- src/test/java/com/guo/practice/codepractice/problems/BinaryTreeTest.java
+++ src/test/java/com/guo/practice/codepractice/problems/BinaryTreeTest.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 @Slf4j
 public class BinaryTreeTest {
 
@@ -13,6 +15,45 @@ public class BinaryTreeTest {
     public void testBinaryTreeInsert() {
         BinaryTree tree = new BinaryTree();
         Node node = tree.insertValue(9);
+
+        Assertions.assertEquals(9, node.getValue());
+    }
+
+    @Test
+    public void testFindValue() {
+        BinaryTree tree = new BinaryTree();
+        tree.insertValue(9);
+        Assertions.assertEquals(9, tree.findValueLoop(9).getValue());
+        Assertions.assertEquals(9, tree.findValueRecursive(9).getValue());
+    }
+
+
+    @Test
+    public void testRemoveValue() {
+        BinaryTree tree = new BinaryTree();
+        tree.insertValue(9);
+        tree.insertValue(4);
+        tree.insertValue(6);
+        tree.insertValue(20);
+        tree.insertValue(170);
+        tree.insertValue(15);
+        tree.insertValue(1);
+        Assertions.assertEquals(9, tree.findValueLoop(9).getValue());
+        Assertions.assertEquals(9, tree.findValueRecursive(9).getValue());
+
+
+        tree.removeValue(170);
+        tree.removeValue(4);
+
+        Assertions.assertNull(tree.findValueLoop(9));
+
+    }
+
+
+    @Test
+    public void testBreadthFirst() {
+        BinaryTree tree = new BinaryTree();
+        tree.insertValue(9);
         tree.insertValue(4);
         tree.insertValue(6);
         tree.insertValue(20);
@@ -20,14 +61,8 @@ public class BinaryTreeTest {
         tree.insertValue(15);
         tree.insertValue(1);
 
-        Assertions.assertEquals(9, node.getValue());
-    }
-
-    @Test
-    public void testFindValue(){
-        BinaryTree tree = new BinaryTree();
-        tree.insertValue(9);
-        Assertions.assertEquals(9, tree.findValueLoop(9).getValue());
-        Assertions.assertEquals(9, tree.findValueRecursive(9).getValue());
+//        List<Integer> list = tree.breadthFirstSearch();
+        List<Integer> list = tree.bfsRecursive();
+        Assertions.assertFalse(list.isEmpty());
     }
 }
