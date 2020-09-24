cf6d3529845d3a9a68547cefa24134934b53ce43
shuaigedebitbucket <guolinxin@gmail.com>
2020-09-24 22:26:25 +0100

    morse code to english

--- src/test/java/com/guo/practice/codepractice/problems/leetcode/topInterview/Hackajob.java
+++ src/test/java/com/guo/practice/codepractice/problems/leetcode/topInterview/Hackajob.java
@@ -14,7 +14,9 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -78,9 +80,9 @@ public class Hackajob {
         /////////////////////////////
         // append sorted list
         characterList = characterList.stream().sorted().collect(Collectors.toList());
-        for(int n = 0; n < characterList.size(); n ++){
+        for (int n = 0; n < characterList.size(); n++) {
             sb.append(characterList.get(n));
-            if(n < characterList.size() -1){
+            if (n < characterList.size() - 1) {
                 sb.append(", ");
             }
         }
@@ -102,17 +104,16 @@ public class Hackajob {
             /////////////////////////////
             // append sorted list
             filmList = filmList.stream().sorted().collect(Collectors.toList());
-            for(int m = 0; m < filmList.size(); m ++){
+            for (int m = 0; m < filmList.size(); m++) {
                 sb.append(filmList.get(m));
-                if(m < filmList.size() -1){
+                if (m < filmList.size() - 1) {
                     sb.append(", ");
                 }
             }
         }
-         log.debug("******** film cha: " + sb.toString());
+        log.debug("******** film cha: " + sb.toString());
 
     }
-
 
 
     public JsonObject getResult(String type, String name) {
@@ -150,6 +151,77 @@ public class Hackajob {
         }
 
         return resultObject;
+    }
+
+    /**
+     * We want you to write a program that converts Morse code to English text and English text to Morse code
+     */
+    @Test
+    public void morseCode() {
+        /*
+         * Write your code below; return type and arguments should be according to the problem's requirements
+         */
+
+        boolean morseToEnglish = true;
+        String textToTranslate = "..- .... ..-. .-.. --- .-    ...-";
+//        textToTranslate = "The wizard quickly jinxed the gnomes before they vaporized.";
+        if (textToTranslate.length() > 1000000 || textToTranslate.length() == 0) {
+//            return "";
+        }
+
+        char[] english = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                ',', '.', '?'};
+
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+                "-----", "--..--", ".-.-.-", "..--.."};
+
+        Map<Character, String> englishMorseMap = new HashMap<>();
+        Map<String, Character> morseEnglishMap = new HashMap<>();
+
+        for (int i = 0; i < 39; i++) {
+            englishMorseMap.put(english[i], morse[i]);
+            morseEnglishMap.put(morse[i], english[i]);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        if (morseToEnglish) {
+            String[] lettes = textToTranslate.split(" ");
+            for (int k = 0; k < lettes.length; k++) {
+                String inputStr = lettes[k];
+                Character cha = morseEnglishMap.get(inputStr);
+                if (inputStr.length() == 0) {
+                    if(lettes[k-1].length() != 0) {
+                        stringBuilder.append(" ");
+                    }
+                } else {
+                    stringBuilder.append(cha.toString());
+                }
+            }
+
+        } else {
+            char[] letters = textToTranslate.toLowerCase().toCharArray();
+            for (int j = 0; j < letters.length; j++) {
+                Character let = letters[j];
+                String code = englishMorseMap.get(let);
+                if (let.toString().equals(" ")) {
+                    stringBuilder.append("  ");
+
+                } else {
+                    stringBuilder.append(code).append(" ");
+                }
+            }
+        }
+
+        String translatedText = stringBuilder.toString();
+
+        System.out.println(translatedText);
+
+//        return translatedText;
     }
 
 
