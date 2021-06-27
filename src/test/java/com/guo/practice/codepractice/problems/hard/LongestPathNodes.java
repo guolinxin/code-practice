9c35a344a34f959a682b5833ebcd6568eea42209
liguo <liguo@theworkshop.com>
2021-06-27 14:16:18 +0100

    practice coding algorithms

---
+++ src/test/java/com/guo/practice/codepractice/problems/hard/LongestPathNodes.java
@@ -0,0 +1,18 @@
+package com.guo.practice.codepractice.problems.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Using the Java language, have the function FarthestNodes(strArr) read strArr which will be an array of hyphenated
+ * letters representing paths between those two nodes. For example: ["a-b","b-c","b-d"] means that there is a path
+ * from node a to b (and b to a), b to c, and b to d. Your program should determine the longest path that exists in the
+ * graph and return the length of that path. So for the example above, your program should return 2 because of the
+ * paths a-b-c and d-b-c. The path a-b-c also means that there is a path c-b-a. No cycles will exist in the graph and
+ * every node will be connected to some other node in the graph.
+ *
+ */
+public class LongestPathNodes {
+
+}
--- src/test/java/com/guo/practice/codepractice/problems/hard/ShortestPathBFS.java
+++ src/test/java/com/guo/practice/codepractice/problems/hard/ShortestPathBFS.java
@@ -29,7 +29,7 @@ public class ShortestPathBFS {
   // 1. build the graph from array
 
   // Node class
-  static class Node {
+  class Node {
     String val;
     int weight;
     Set<Node> adjacentList = new HashSet<>();
@@ -42,8 +42,8 @@ public class ShortestPathBFS {
   // Storage save nodes
   Map<String, Node> nodes = new HashMap<>();
 
-  // add edge / adjacent
-  public void addEdge(String start, String end) {
+  // add connection / adjacent
+  public void addConnection(String start, String end) {
     Node s = nodes.get(start);
     Node e = nodes.get(end);
     s.adjacentList.add(e);
@@ -62,7 +62,7 @@ public class ShortestPathBFS {
     // add connection
     for (int j = nodeSize + 1; j < input.length; j++) {
       String[] connection = input[j].split("-");
-      addEdge(connection[0], connection[1]);
+      addConnection(connection[0], connection[1]);
     }
   }
 
@@ -70,62 +70,77 @@ public class ShortestPathBFS {
 
   public List<String> shortestPathBFS(String start, String end) {
 
-    Node sNode = nodes.get(start);
-    Node eNode = nodes.get(end);
+    Node startNode = nodes.get(start);
+    Node endNode = nodes.get(end);
 
-    // queue to loop graph
+    // create queue to loop graph
     Queue<Node> nextTOVisit = new LinkedList<>();
-    // store visited
-    //        Set<String> visited = new HashSet<>();
+    // create map store visited nodes
     Map<String, Node> visited = new HashMap<>();
+    // add startNode
+    nextTOVisit.add(startNode);
 
-    // start node
-    nextTOVisit.add(sNode);
-
+    // BFS
     while (!nextTOVisit.isEmpty()) {
-      Node visitedNode = nextTOVisit.remove();
-
-      if (visitedNode == eNode) {
-        log.info("*** End founded ***");
-        visited.put(visitedNode.val, visitedNode);
-        return buildShortestPath(visited, sNode, eNode);
-      }
-
+      Node currentVisit = nextTOVisit.remove();
       // continue loop if visited before
-      if (visited.get(visitedNode.val) != null) {
+      if (visited.get(currentVisit.val) != null) {
         continue;
       }
-      visited.put(visitedNode.val, visitedNode);
+      visited.put(currentVisit.val, currentVisit);
 
       // add adjacent to queue
-      for (Node adjNode : visitedNode.adjacentList) {
-        if (!adjNode.val.equals(sNode.val)) {
-          adjNode.weight = visitedNode.weight + 1;
+      for (Node adjNode : currentVisit.adjacentList) {
+        if (!adjNode.val.equals(startNode.val) && !visited.containsKey(adjNode.val)) {
+          adjNode.weight = currentVisit.weight + 1;
         }
         nextTOVisit.add(adjNode);
       }
+
+      // calculate shortest path if end node found
+      if (currentVisit == endNode) {
+        log.info("*** End founded ***");
+        return buildShortestPath(startNode, endNode);
+      }
+    }
+
+    // return empty if end node not been visited
+    if (!visited.isEmpty() && !visited.containsKey(end)) {
+      return Collections.emptyList();
     }
 
     return Collections.emptyList();
   }
 
-  private List<String> buildShortestPath(Map<String, Node> visited, Node sNode, Node eNode) {
+  private List<String> buildShortestPath(Node startNode, Node endNode) {
+    // reverse check nodes have least weight to start nodes
     List<String> shortestPath = new ArrayList<>();
-    Node current = eNode;
+    Node current = endNode;
     while (current != null) {
+      // add to shortest path list
       shortestPath.add(current.val);
+      // get least weight adjacent
       current = getNextLeastWeightAdjacent(current);
+      if (current.val.equals(startNode.val)) {
+        shortestPath.add(current.val);
+        break;
+      }
     }
     return shortestPath;
   }
 
   private Node getNextLeastWeightAdjacent(Node node) {
+    Node lessWeightNode = null;
     for (Node adj : node.adjacentList) {
-      if (adj.weight == node.weight - 1) {
-        return adj;
+      if (lessWeightNode == null) {
+        lessWeightNode = adj;
+      } else {
+        if (adj.weight < lessWeightNode.weight) {
+          lessWeightNode = adj;
+        }
       }
     }
-    return null;
+    return lessWeightNode;
   }
 
   public String shortestPath(String[] input, String start, String dest) {
@@ -133,8 +148,12 @@ public class ShortestPathBFS {
     createNodes(input);
     List<String> list = shortestPathBFS(start, dest);
 
+    if (list.isEmpty()) {
+      return "-1";
+    }
+
     StringBuilder stringBuilder = new StringBuilder();
-    for (int i = list.size()-1; i >=0; i--) {
+    for (int i = list.size() - 1; i >= 0; i--) {
       stringBuilder.append(list.get(i));
       if (i > 0) {
         stringBuilder.append("-");
@@ -147,7 +166,11 @@ public class ShortestPathBFS {
   @Test
   public void testShortestPath() {
     String[] s = new String[] {"4", "A", "B", "C", "D", "A-B", "B-D", "B-C", "C-D"};
-    String str = shortestPath(s, "A", "D");
+
+    ShortestPathBFS shortestPathBFS = new ShortestPathBFS();
+    String str = shortestPathBFS.shortestPath(s, "A", "D");
+
+    //    String str = shortestPath(s, "A", "D");
 
     log.info(str);
   }
